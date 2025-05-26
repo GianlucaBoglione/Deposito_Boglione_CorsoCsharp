896a9dccdf1559d3f9171289154a85ec80ff2090
GianlucaBoglione <boglione.gianluca@gmail.com>
2025-05-26 16:13:41 +0200

    Add files via upload

---
+++ interfaccia/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+public interface IPagamento
+{
+    void EseguiPagamento(decimal importo);
+    void MostraMetodo();
+}
+public class PagamentoCarta : IPagamento
+{
+    private string circuito;
+
+    public string Circuito { get; set; }
+
+    public PagamentoCarta(string circuito)
+    {
+        this.circuito = circuito;
+    }
+
+    public void EseguiPagamento(decimal importo)
+    {
+        Console.WriteLine($"Importo: {importo} euro, Pagato con carta (circuito: {circuito})");
+    }
+
+    public void MostraMetodo()
+    {
+        Console.WriteLine("Metodo: carta di credito");
+    }
+}
+ public class PagamentoPayPal : IPagamento
+    {
+        private string EmailUtente{ get; set; }
+
+        public PagamentoPayPal(string emailUtente)
+        {
+            this.EmailUtente = emailUtente;
+        }
+
+        public void EseguiPagamento(decimal importo)
+        {
+            Console.WriteLine($"Importo: {importo} euro, Pagato con carta (circuito: {EmailUtente})");
+        }
+
+        public void MostraMetodo()
+        {
+            Console.WriteLine("Metodo: carta di credito");
+        }
+    }
+public class PagamentoContanti : IPagamento
+{
+        public void EseguiPagamento(decimal importo)
+    {
+        Console.WriteLine($"Importo: {importo} euro, Pagato con PayPal)");
+    }
+
+    public void MostraMetodo()
+    {
+        Console.WriteLine("Metodo: PayPal");
+    }
+    
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<IPagamento> pagamento = new List<IPagamento>();
+
+        decimal importo = 0;
+        bool continua = true;
+        // inizio menu 
+        while (continua)
+        {
+            Console.WriteLine("Scegli il metodo di pagamento: ");
+            Console.WriteLine("[1] Pagamento con carta di credito");
+            Console.WriteLine("[2] pagamento con payPal");
+            Console.WriteLine("[3] Pagamento in contanti");
+            Console.WriteLine("[4] Visualizza");
+            Console.WriteLine("[5] Esci");
+
+            int scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1: // Pagamento con Carta di Credito
+                    Console.Write("Inserisci il circuito della carta (es. Visa, MasterCard): ");
+                    string circuitoCarta = Console.ReadLine();
+                    PagamentoCarta carta = new PagamentoCarta(circuitoCarta);
+                    pagamento.Add(carta);
+                    Console.WriteLine("Metodo 'Pagamento con Carta' aggiunto.");
+                    break;
+                case 2: // Pagamento con PayPal
+                    Console.Write("Inserisci l'email dell'account PayPal: ");
+                    string emailPayPal = Console.ReadLine();
+                    PagamentoPayPal paypal = new PagamentoPayPal(emailPayPal);
+                    pagamento.Add(paypal);
+                    Console.WriteLine("Metodo 'Pagamento con PayPal' aggiunto.");
+                    break;
+
+                case 3: // Pagamento in Contanti
+                    PagamentoContanti contanti = new PagamentoContanti();
+                    pagamento.Add(contanti);
+                    Console.WriteLine("Metodo 'Pagamento in Contanti' aggiunto.");
+                    break;
+                case 4: // visualizzazione 
+                    foreach (IPagamento metodo in pagamento)
+                    {
+                        metodo.MostraMetodo();
+                        metodo.EseguiPagamento(importo);
+                    }
+                    break;
+                    case 5:
+                        continua = false;
+                        Console.WriteLine("Uscita dal programma. Arrivederci!");
+                        break;
+
+                default: // Scelta non valida
+                    Console.WriteLine("Scelta non valida. Riprova.");
+                    break;
+            } while (continua) ;
+        }
+
+    }
+}
+    
---
+++ laboratorioTecnico/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class DispositivoElettronico
+{
+    private string _modello; // Corretto il nome del campo privato per evitare conflitti
+
+    public string Modello { get; set; }
+
+    // Costruttore della classe base
+    public DispositivoElettronico(string modello)
+    {
+        Modello = modello;
+    }
+
+    // Metodi astratti che devono essere implementati dalle classi derivate
+    public abstract void Accendi();
+    public abstract void Spegni();
+    public virtual void MostraInfo()
+    {
+        Console.WriteLine($"Modello: {Modello}");
+    }
+}
+public class Computer : DispositivoElettronico
+{
+    public Computer(string modello) : base(modello) { }
+
+// metodo implementato 
+    public override void Accendi()
+    {
+        Console.WriteLine("Il computer si avvia.....");
+    }
+    // metodo implementato 
+    public override void Spegni()
+    {
+        Console.WriteLine("Il computer si spenge");
+    }
+    public override void MostraInfo()
+    {
+        Console.WriteLine($"Modello: {Modello}");
+    }
+}
+
+public class Stampante : DispositivoElettronico
+{
+    public Stampante(string modello) : base(modello) { }
+    
+    // metodo implementato 
+    public override void Accendi()
+    {
+        Console.WriteLine("La stampante si accende.");
+    }
+    // metodo implementato 
+    public override void Spegni()
+    {
+        Console.WriteLine("La stampante va in standby");
+    }
+    public override void MostraInfo()
+    {
+        Console.WriteLine($"Modello: {Modello}");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<DispositivoElettronico> dispElettronico = new List<DispositivoElettronico>();
+
+        bool continua = true;
+        // inizio menu 
+        while (continua)
+        {
+            Console.WriteLine("Scegli una delle opzioni: ");
+            Console.WriteLine("[1] Aggiungi computer");
+            Console.WriteLine("[2] Aggiungi Stampante");
+            Console.WriteLine("[3] Mostra informazioni");
+            Console.WriteLine("[4] Esci");
+
+            int scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    Console.WriteLine("Inserisci il modello del computer: ");
+                    string modelloC = Console.ReadLine();
+                    dispElettronico.Add(new Computer(modelloC));
+                    break;
+                case 2:
+                    Console.WriteLine("Inserisci il modello della stampante: ");
+                    string modelloS = Console.ReadLine();
+                    dispElettronico.Add(new Stampante(modelloS));
+                    break;
+                case 3:
+                    foreach (DispositivoElettronico dispositivo in dispElettronico)
+                    {
+                        dispositivo.MostraInfo();
+                        dispositivo.Accendi();
+                        dispositivo.Spegni();
+                    }
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Scelta non valida ");
+                    break;
+            }
+        }
+    }
+}
+
